c66479f0cc04bb44ba19e1547316a3f53756c057
ansh-raghuwanshi <anshraghuwansh12@gmail.com>
2026-05-20 01:40:51 +0530

    fix: dynamically allow all CORS origins

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieparser from 'cookie-parser'
 
 const app=express();
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  origin: true,
   credentials: true
 }))
 
